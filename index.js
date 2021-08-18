b0c1f5c1b1afdea05cb375b9ee73128424a6faae
eagle <eagle.lam.le@Gmail.com>
2021-08-18 16:26:46 -0400

    function add

--- index.js
+++ index.js
@@ -124,3 +124,57 @@ function viewEmployeeByDepartment() {
     });
 
 }
+function addEmployee() {
+    console.log("Inserting an employee!");
+
+    var query = `SELECT r.id, r.title, r.salary FROM role r`
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        const roleChoices = res.map(({ id, title, salary }) => ({
+            value: id,
+            title: `${title}`,
+            salary: `${salary}`
+        }));
+
+
+        inquirer
+            .prompt([{
+                    type: "input",
+                    name: "first_name",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    type: "input",
+                    name: "last_name",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    type: "list",
+                    name: "roleId",
+                    message: "What is the employee's role?",
+                    choices: roleChoices
+                },
+
+            ])
+            .then(function(answer) {
+                console.log(answer);
+
+                var query = `INSERT INTO employee SET ?`
+                connection.query(query, {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    role_id: answer.roleId,
+                    manager_id: answer.managerId,
+                }, (err, res) => {
+                    if (err) throw err;
+
+                    console.table(res);
+
+                    menuChoices();
+                });
+            });
+
+    });
+}
